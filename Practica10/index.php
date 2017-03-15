e48d0f3aaa21a563f0083232dd6f6d81c2809cab
LuisMartinezLSC <jose_luisLSC@hotmail.com>
2017-03-14 20:13:59 -0600

    Add files via upload

---
+++ Practica10/index.php
@@ -0,0 +1,18 @@
+<?php
+//Comentario
+#comentario
+/* comentario 
+multilinea*/ 
+echo "Hola mundo php";
+?>
+<html>
+<head>
+<title> Practica 10 PHP </title>
+</head>
+
+<body>
+
+<h1>Hola Mundo Html</h1>
+
+</body>
+</html>
---
+++ Practica10/index2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> Practica 10 PHP </title>
+</head>
+
+<body>
+
+<h1>Usando PHP</h1>
+	<p><?php 
+	echo ="Parrafo php";
+	?></p>
+	<h3>Parrafo HTML</h3>
+		<h3><?php 
+	echo ="Parrafo php";
+	?></h3>
+</body>
+</html>
